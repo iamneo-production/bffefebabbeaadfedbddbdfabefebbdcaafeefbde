6d286300f9ad40918eefa3845360225b6f4110d5
root <root@bffefebabbeaadfedbddbdfabefebbdcaafeefbde-0.bffefebabbeaadfedbddbdfabefebbdcaafeefbde.che.svc.cluster.local>
2023-03-27 04:46:23 +0000

    Backup Branch before deleting

---
+++ jspservlet-app-bmi-calculator/src/main/java/controller/CalculateBMI.java
@@ -0,0 +1,24 @@
+package controller;
+
+public class CalculateBMI {
+
+	//Enter the code here...
+	CalculateBMI(){
+
+	}
+public static double calculate(double height,double weight){
+	return weight/height;
+}
+public static String description(double bmi){
+	if(bmi<18.5)
+	  return "Under Weight";
+	else if(bmi>=18.5 && bmi<24.9)
+	  return "Normal";
+	else if(bmi>=25&&bmi<24.9)
+	 return "Over Weight";
+	else if(bmi>=30&&bmi<34.9)
+	 return "Obese";
+	else 
+	 return "Extremely Obese";
+    }
+}
